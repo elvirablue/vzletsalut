a6df18538ef3a1c0b0813d847ffd0109cb6ba628
Olga Tretyakova <o.tretyakova@bc.ru>
2023-08-29 12:50:41 +0400

    30

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,29 +1,28 @@
 
     function loaded() {
-        document.querySelector('#buttonOpenAsideFilters')?.addEventListener('click', () => openAside('asideFilters'))
-        document.querySelector('#buttonCloseAsideFilters')?.addEventListener('click', () => closeAside('asideFilters'))
+        document.body.querySelector('#buttonOpenAsideFilters')?.addEventListener('click', () => openAside('asideFilters'))
+        document.body.querySelector('#buttonCloseAsideFilters')?.addEventListener('click', () => closeAside('asideFilters'))
 
-        document.querySelector('#buttonOpenAsideProfile')?.addEventListener('click', () => openAside('asideProfile'))
-        document.querySelector('#closeAsideProfile')?.addEventListener('click', () => closeAside('asideProfile'))
+        document.body.querySelector('#buttonOpenAsideProfile')?.addEventListener('click', () => openAside('asideProfile'))
+        document.body.querySelector('#closeAsideProfile')?.addEventListener('click', () => closeAside('asideProfile'))
 
-        document.querySelector('#buttonOpenNavigationMobile')?.addEventListener('click', openNavigationMobile)
-        document.querySelector('#buttonCloseNavigationMobile')?.addEventListener('click', closeNavigationMobile)
+        document.body.querySelector('#buttonOpenNavigationMobile')?.addEventListener('click', openNavigationMobile)
+        document.body.querySelector('#buttonCloseNavigationMobile')?.addEventListener('click', closeNavigationMobile)
+        document.body.querySelector('#buttonCloseCatalog')?.addEventListener('click', closeCatalog)
 
-        document.querySelector('#buttonOpenContactsModal')?.addEventListener('click', openContactsModal)
+        document.body.querySelector('#buttonOpenContactsModal')?.addEventListener('click', openContactsModal)
 
-
-
-        const buttonCatalogMenu = document.querySelector('#buttonCatalogMenu')
+        const buttonCatalogMenu = document.body.querySelector('#buttonCatalogMenu')
         buttonCatalogMenu?.addEventListener('click', openCatalog)
         buttonCatalogMenu?.addEventListener('mouseenter', openCatalog)
         buttonCatalogMenu?.addEventListener('mouseleave', catalogButtonMouseLeave)
 
-        window.addEventListener('resize', closeAll )
-        const headerHeight = document.querySelector('.app-header')?.clientHeight ?? 0;
-        const breadcrumbsHeight = document.querySelector('.app-breadcrumbs')?.clientHeight ?? 0;
+        window.addEventListener('resize', closeAll)
+        const headerHeight = document.body.querySelector('.app-header')?.clientHeight ?? 0;
+        const breadcrumbsHeight = document.body.querySelector('.app-breadcrumbs')?.clientHeight ?? 0;
         document.body.style.setProperty('--header-height', (`${headerHeight + breadcrumbsHeight}px`))
 
-        const sliderSections = document.querySelectorAll(".range-slider")
+        const sliderSections = document.body.querySelectorAll(".range-slider")
         if (sliderSections.length) {
             for (let x = 0; x < sliderSections.length; x++) {
                 let sliders = sliderSections[x].getElementsByTagName("input");
@@ -39,7 +38,7 @@
     }
 
     function getHeaderHeight() {
-        const headerHeight = document.querySelector('.header-top').clientHeight || 0;
+        const headerHeight = document.body.querySelector('.header-top').clientHeight || 0;
         document.body.style.setProperty('--header-height', (`${headerHeight}px`))
     }
 
@@ -59,10 +58,12 @@
         } else {
             elementAside = document.body.querySelector('.aside.-open.-show')
         }
-        elementAside.classList.remove('-open');
-        setTimeout(() => {
-            elementAside.classList.remove('-show');
+        if (elementAside) {
+            elementAside.classList.remove('-open');
+            setTimeout(() => {
+                elementAside.classList.remove('-show');
             }, 250)
+        }
 
     }
 
@@ -79,10 +80,8 @@
     }
 
     function openCatalog() {
-        const catalog = document.body.querySelector('#catalog')
-        const catalogButton = document.body.querySelector('#catalogButton')
-        catalog.classList.add('-show')
-        catalogButton.querySelector('.button-accent').classList.add('-active')
+        document.body.querySelector('#catalog')?.classList.add('-show')
+        document.body.querySelector('#buttonCatalogMenu')?.classList.add('-active')
     }
 
     function trackCursorOnCatalog(event) {
@@ -94,9 +93,11 @@
 
     function closeCatalog() {
         const catalog = document.body.querySelector('#catalog')
-        catalog.classList.remove('-show')
-        document.body.querySelector('.button-accent.-active')?.classList?.remove('-active')
-        document.body.removeEventListener( 'mousemove', trackCursorOnCatalog)
+        if (catalog) {
+            catalog.classList.remove('-show')
+            document.body.querySelector('.button-accent.-active')?.classList?.remove('-active')
+            document.body.removeEventListener('mousemove', trackCursorOnCatalog)
+        }
     }
     function catalogButtonMouseLeave(event) {
         setTimeout(() => {
@@ -109,8 +110,8 @@
         getHeaderHeight()
         closeAside()
 
-        const elementNavigationWrap = document.querySelector('#navigationWrap');
-        const elementNavigationContent = document.querySelector('#navigationMobile');
+        const elementNavigationWrap = document.body.querySelector('#navigationWrap');
+        const elementNavigationContent = document.body.querySelector('#navigationMobile');
         elementNavigationWrap.classList.add('-show');
         elementNavigationContent.classList.add('-show');
         setTimeout(() => {
@@ -121,15 +122,18 @@
 
     function closeNavigationMobile() {
         closeCatalog()
-        const elementNavigationWrap = document.querySelector('#navigationWrap');
-        elementNavigationWrap.classList.remove('-open');
-        const elementNavigationContent = document.querySelector('#navigationMobile');
-        elementNavigationContent.classList.remove('-move');
-        setTimeout(() => {
-            elementNavigationWrap.classList.remove('-show');
-            elementNavigationContent.classList.remove('-show');
-            document.body.classList.remove('--hidden');
-        }, 500)
+        const elementNavigationWrap = document.body.querySelector('#navigationWrap');
+        const elementNavigationContent = document.body.querySelector('#navigationMobile');
+
+        if (elementNavigationWrap && elementNavigationContent) {
+            elementNavigationWrap.classList.remove('-open');
+            elementNavigationContent.classList.remove('-move');
+            setTimeout(() => {
+                elementNavigationWrap.classList.remove('-show');
+                elementNavigationContent.classList.remove('-show');
+                document.body.classList.remove('--hidden');
+            }, 500)
+        }
     }
 
     function getValuesRange(slider){
@@ -161,35 +165,44 @@
     }
 
     function openContactsModal() {
-        const template = document.querySelector('#formContacts');
-        const clone = template.content.cloneNode(true);
-        openModal(clone);
+        console.debug('************** click openContactsModal')
+        const template = document.body.querySelector('#formContacts');
+        if (template) {
+            const clone = template.content.cloneNode(true);
+            openModal(clone);
+        }
     }
 
     function openModal(clone) {
-        document.body.classList.add('--hidden');
-        const elementModal = document.querySelector('#modal');
-        const elementModalOuter = document.querySelector('#modalOuter');
-        elementModalOuter?.addEventListener('click', closeModal);
-        const modalContent = document.querySelector('#modalContent');
-        modalContent.appendChild(clone);
-        elementModal.classList.add('-show');
-        setTimeout(() => {
-            elementModal.classList.add('-animate');
-        }, 20)
+        const elementModal = document.body.querySelector('#modal');
+        if (elementModal) {
+            const elementModalOuter = elementModal.querySelector('#modalOuter');
+            const modalContent = elementModal.querySelector('#modalContent');
+            elementModal.querySelector('#buttonModalClose')?.addEventListener('click', closeModal)
+
+            document.body.classList.add('--hidden');
+            elementModalOuter.addEventListener('click', closeModal);
+            modalContent.appendChild(clone);
+            elementModal.classList.add('-show');
+            setTimeout(() => {
+                elementModal.classList.add('-animate');
+            }, 20)
+        }
     }
 
     function closeModal() {
-        const elementModal = document.querySelector('#modal');
-        elementModal.classList.remove('-animate');
-        setTimeout(() => {
-            elementModal.classList.remove('-show');
-            document.body.classList.remove('--hidden');
-        }, 500)
-        setTimeout(() => {
-            const modalContent = document.querySelector('#modalContent');
-            modalContent.innerHTML = '';
-        }, 800)
+        const elementModal = document.body.querySelector('#modal');
+        if (elementModal) {
+            elementModal.classList.remove('-animate');
+            setTimeout(() => {
+                elementModal.classList.remove('-show');
+                document.body.classList.remove('--hidden');
+            }, 500)
+            setTimeout(() => {
+                const modalContent = document.body.querySelector('#modalContent');
+                modalContent.innerHTML = '';
+            }, 800)
+        }
     }
 
     function closeAll() {
